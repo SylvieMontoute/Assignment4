790d6895f2840d6a513a7282c8643c0b296638eb
Sylvain <sylviemontoute@gmail.com>
2023-06-18 13:44:50 -0400

    second commit

---
+++ task2.py
@@ -0,0 +1,4 @@
+FirstName="Sylvain"
+MiddleName="Krystel"
+LastName="Montoute"
+print("My first name is {}, my middle name is {} and my last name is {}.".format(FirstName,MiddleName,LastName))
---
+++ task3.py
@@ -0,0 +1,33 @@
+x=5
+type_x= type(x)
+print(f"the value of x is {x} and the datatype of x is {type_x} ")
+x=3.25
+type_x= type(x)
+print(f"the value of x is {x} and the datatype of x is {type_x}")
+x=complex(5,6)
+type_x= type(x)
+print(f"the value of x is {x} and the datatype of x is {type_x}")
+x="apple"
+type_x= type(x)
+print(f"the value of x is {x} and the datatype of x is {type_x}")
+x=["apples","bananas","mangoes"]
+type_x= type(x)
+print(f"the value of x is {x} and the datatype of x is {type_x}")
+x=("apples","bananas","mangoes")
+type_x= type(x)
+print(f"the value of x is {x} and the datatype of x is {type_x}")
+x=range(4)
+type_x= type(x)
+print(f"the value of x is {x} and the datatype of x is {type_x}")
+x={"name":"Sylvain", "age":35, "Year of birth":1988}
+type_x= type(x)
+print(f"the value of x is {x} and the datatype of x is {type_x}")
+x={"Sylvain",35,1988}
+type_x= type(x)
+print(f"the value of x is {x} and the datatype of x is {type_x}")
+x=bool(True)
+type_x= type(x)
+print(f"the value of x is {x} and the datatype of x is {type_x}")
+x=b"apples"
+type_x= type(x)
+print(f"the value of x is {x} and the datatype of x is {type_x}")
